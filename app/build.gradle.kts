949b4da8539f052892c9e5fe8f1c348d2d8066b5
TyrEamon <yuanbin200409@gmail.com>
2026-05-14 10:35:34 +0800

    Update CI compile SDK to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
 
 android {
     namespace = "io.github.yueeng.hacg"
-    compileSdk = 35
-    buildToolsVersion = "35.0.0"
+    compileSdk = 36
+    buildToolsVersion = "36.0.0"
 
     defaultConfig {
         applicationId = "io.github.yueeng.hacg"
